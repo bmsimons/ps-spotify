e338b663674d5f4852a5b370b48b8675f5a16493
Thomas Malkewitz <dotps1@outlook.com>
2016-09-30 18:35:46 -0400

    Added check when module is imported to ensure that Spotify is installed and running.

--- ps-spotify/ps-spotify.psm1
+++ ps-spotify/ps-spotify.psm1
@@ -1,6 +1,19 @@
+# Test if Spotify is installed.
 if (-not (Test-Connection -ComputerName "www.spotilocal.com" -Count 1 -Quiet)) {
     Write-Error -Message "Spotify is not installed."
+    $_
     return
+}
+
+# Launch spotify if its not running.
+if (-not (Get-Process -Name "Spotify" -ErrorAction SilentlyContinue)) {
+    try {
+        Start-Process -FilePath "$env:APPDATA\Spotify\Spotify.exe" -ErrorAction Stop -PassThru
+    } catch {
+        Write-Error -Message "Failed to start Spotify."
+        $_
+        return
+    }
 }
 
 $paths = @(
